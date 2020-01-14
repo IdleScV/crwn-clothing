422c0c697a84dbc27345e3904ad6f0803e0a9314
Wesley Chen <weschen.github@gmail.com>
2020-01-14 16:15:56 -0500

    Added firebase
    yarn added firebase
    created authentication on firebase website using googleAuth
    added google sign in button in sign-in page

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCx_Wpf5T_ztItDdllztjsLsuorrUlRqG8",
+    authDomain: "crwn-db-3a814.firebaseapp.com",
+    databaseURL: "https://crwn-db-3a814.firebaseio.com",
+    projectId: "crwn-db-3a814",
+    storageBucket: "crwn-db-3a814.appspot.com",
+    messagingSenderId: "33422990951",
+    appId: "1:33422990951:web:d8e13fff36743edeb47ec2",
+    measurementId: "G-K1EENJZFHK"
+  }
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
